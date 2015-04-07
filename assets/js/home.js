1a977a84505efdced4bf79a0eb7fa868717ce0f9
Hamza Essaoui <hamza@flickerbox.com>
2015-04-06 18:37:57 -0700

    Update day
    Change moment js day

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,4 +1,4 @@
 $(function() {
-  var current_date_format = moment().format('ddd MMM MM YYYY, HH:MM:ss');
+  var current_date_format = moment().format('ddd MMM DD YYYY, HH:MM:ss');
   $('#last-login-date').text(current_date_format);
 });
