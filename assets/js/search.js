b2f6f8f11cb871aaa3467308219456841d4ad12e
Joel Franusic <joel@franusic.com>
2016-08-31 11:37:02 -0700

    Add to static HTML

--- assets/js/search.js
+++ assets/js/search.js
@@ -1,3 +1,11 @@
-$(document).ready(function() {
-     
-});
+    var initSearch = function() {
+	var cx = '005121479574088032773:mmh_vhv8uns';
+	var gcse = document.createElement('script');
+	gcse.type = 'text/javascript';
+	gcse.async = true;
+	gcse.src = (document.location.protocol == 'https:' ? 'https:' : 'http:') +
+	    '//cse.google.com/cse.js?cx=' + cx;
+	var s = document.getElementsByTagName('script')[0];
+	s.parentNode.insertBefore(gcse, s);
+    }
+    initSearch();
