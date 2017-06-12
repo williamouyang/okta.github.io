c31d4f0486c59cfda39614ddd11a2e6590938a95
Vijet M <vijet.mahabaleshwar@okta.com>
2017-06-12 09:26:36 -0700

    Removing the beta tag validation on “/docs/api/resources/oauth2.html” page. (#942)
    This is failing travis test on Mysti's doc change (OKTA-124731)

--- tests/selenium/spec/api-tags-spec.js
+++ tests/selenium/spec/api-tags-spec.js
@@ -12,7 +12,8 @@ describe('API tags check spec', () => {
   it('shows the Beta and Early Access lifecycle tags', () => {
     const docsPage = new DocsPage('/docs/api/resources/oauth2.html');
     docsPage.load();
-    expect(docsPage.hasBetaTags()).toBe(true);
+    // TODO - Look for beta tags in other pages. Beta tag on this page updated to EA (OKTA-124731)
+    //expect(docsPage.hasBetaTags()).toBe(true);
     expect(docsPage.hasEATags()).toBe(true);
     expect(docsPage.hasDeprecatedTags()).toBe(false);
   });
