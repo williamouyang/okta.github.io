e6350ca78cd7a492201bf87cd5c7b247c8225403
Vijet M <vijet.mahabaleshwar@okta.com>
2017-06-12 10:16:34 -0700

    Using the lifecycle operations page for tag checks (#943)
    Removed checks from other pages, since these tags will be changed, eventually.

--- tests/selenium/spec/api-tags-spec.js
+++ tests/selenium/spec/api-tags-spec.js
@@ -1,30 +1,18 @@
 const DocsPage = require('../framework/page-objects/DocsPage');
 
 describe('API tags check spec', () => {
-  it('shows the Deprecated lifecycle tags', () => {
-    const docsPage = new DocsPage('/docs/api/resources/sessions.html');
+  it('shows the Beta, Early Access and Deprecated lifecycle tags', () => {
+    const docsPage = new DocsPage('/docs/api/getting_started/releases-at-okta.html');
     docsPage.load();
-    expect(docsPage.hasDeprecatedTags()).toBe(true);
-    expect(docsPage.hasBetaTags()).toBe(false);
-    expect(docsPage.hasEATags()).toBe(false);
-  });
-
-  it('shows the Beta and Early Access lifecycle tags', () => {
-    const docsPage = new DocsPage('/docs/api/resources/oauth2.html');
-    docsPage.load();
-    // TODO - Look for beta tags in other pages. Beta tag on this page updated to EA (OKTA-124731)
-    //expect(docsPage.hasBetaTags()).toBe(true);
+    expect(docsPage.hasBetaTags()).toBe(true);
     expect(docsPage.hasEATags()).toBe(true);
-    expect(docsPage.hasDeprecatedTags()).toBe(false);
+    expect(docsPage.hasDeprecatedTags()).toBe(true);
   });
 
   it('shows the CORS tags', () => {
     const docsPage = new DocsPage('/docs/api/getting_started/enabling_cors.html ');
     docsPage.load();
     expect(docsPage.hasCORSTags()).toBe(true);
-    expect(docsPage.hasDeprecatedTags()).toBe(false);
-    expect(docsPage.hasBetaTags()).toBe(false);
-    expect(docsPage.hasEATags()).toBe(false);
   });
 
   it('shows the API URI tags', () => {
