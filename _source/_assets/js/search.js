1973fb47d97b398c374be1828c5830b8b097294f
hamza <hamza@flickerbox.com>
2017-06-22 10:08:51 -0700

    Testing Swiftype

--- _source/_assets/js/search.js
+++ _source/_assets/js/search.js
@@ -1,13 +1,56 @@
+//= require swiftype/jquery.ba-hashchange.min
+//= require swiftype/jquery.swiftype.search
+//= require swiftype/jquery.swiftype.autocomplete
+
+
 $(function () {
 
-	var cx = '005121479574088032773:mmh_vhv8uns';
-	var gcse = document.createElement('script');
+	if (window.location.href.indexOf("?stq=") > -1) {
+		var q = getUrlVars()["stq"];
+		$("#st-search-input").val(q);
+		$("#st-search-input").attr('placeholder', q);
+		window.location.href = window.location.href.replace("?stq=", '#stq=');
+		return false;
+	}
 
-	gcse.type = 'text/javascript';
-	gcse.async = true;
-	gcse.src = document.location.protocol + '//cse.google.com/cse.js?cx=' + cx;
+	// $('#st-search-input').swiftypeSearch({
+ 	// 	resultContainingElement: '#st-results-container',
+ 	// 	renderFunction: oktaDefaultRenderFunction,
+ 	// 	engineKey: 'VoUosPoJvtAtkm68Cd-_',
+ 	// 	perPage: 20
+	// });
 
-	var s = document.getElementsByTagName('script')[0];
-	s.parentNode.insertBefore(gcse, s);
+	// $('#st-search-input').swiftype({
+	//   engineKey: 'VoUosPoJvtAtkm68Cd-_',
+	//   renderFunction: oktaCustomRenderFunction,
+	//   resultLimit: 10,
+	// });
 
+	// var oktaDefaultRenderFunction = function(document_type, item) {
+	// 	console.log(item);
+
+	//   //return '<div class="st-result"><h3 class="title"><a href="' + item['url'] + '" class="st-search-result-link">' + item['title'] + '</a></h3></div>';
+	// 	return '<a href="' + item['url'] + '"> <span class="st-ui-type-heading">' + item['title'] + '</span> <span class="st-ui-type-detail">  <span class="st-ui-type-detail-bold">' + item['url'] + '</span>'+ item['body']  +' </span>	</a>';
+	// };
+
+	// var oktaCustomRenderFunction = function(document_type, item) {
+	// 	console.log(item);
+	// 	return '<a href="' + item['url'] + '"> <span class="st-ui-type-heading">' + item['title'] + '</span> <span class="st-ui-type-detail">  <span class="st-ui-type-detail-bold">' + item['url'] + '</span>'+ item['body']  +' </span>	</a>';
+	// 	// var out = '<div class="st-result"><h3 class="title"><a href="' + item['url'] + '" class="st-search-result-link">' + item['title'] + '</a></h3></div>';
+	// 	return out;
+	// };
+
+
+	function getUrlVars()
+	{
+		var vars = [], hash;
+		var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
+		for(var i = 0; i < hashes.length; i++)
+		{
+			hash = hashes[i].split('=');
+			vars.push(hash[0]);
+			vars[hash[0]] = hash[1];
+		}
+		return vars;
+	}
 });
